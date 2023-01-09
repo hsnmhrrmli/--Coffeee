8636555ea0adecaa1b03f059f6d54cff83069e63
hsnmhrrmli <hsnmhrrmli@gmail.com>
2023-01-09 13:40:49 +0400

    added

---
+++ News page landing/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { navbar } from './navbar/Navbar'
+export const App = () => {
+    console.log("first")
+    return (
+    
+    <>
+
+    <div>Appefwefwefwefwefwefwefweeeeeefiuqwfiqwiuvw</div>
+    {/* <Navbar/> */}
+    </>
+  )
+}  
